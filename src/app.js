b53182c73ae4e5999baa8849c78a3d23187a7a12
Surya <Surya2in1@gmail.com>
2023-02-08 11:30:27 +0530

    Handle bars

--- src/app.js
+++ src/app.js
@@ -6,23 +6,3 @@ app.listen(port,()=>{
     console.log(`This is port ${port}`)
 })
 
-app.get('/',(req,res)=>{
-    res.write('<h1>Welcome to Homepage');
-    res.send();
-});
-
-app.get('/tmp',(req,res)=>{
-    res.send([{
-        name:"Suryakant",
-        age: 36
-    }
-    ])
-})
-
-app.get('/test',(req,res)=>{
-    res.json([{
-        id:1,
-        name:'Suryakant'
-    }])
-
-})
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const staticPath = path.join(__dirname, '../public');
-console.log(staticPath);
-
-// Build in middleware
-app.use(express.static(staticPath));
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// set template engine
+app.set("view engine","hbs");// you can use ejs, pub
+
+app.get("",(req,res)=>{
+    res.render("index",{channelName:"Surya"}); // pointing to views->index.hbs 
+});
